113d3a84989e6381ded29deecd6d91eb54a06d37
yadav-deepakk <deepakyadav53882@gmail.com>
2025-08-19 23:56:00 +0530

    working on employee apis

---
+++ layer-app/src/main/java/io/ctws/layer_app/pojo/Employee.java
@@ -0,0 +1,20 @@
+package io.ctws.layer_app.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+	
+	private long id; 
+	private String name; 
+	private String email; 
+
+}
---
+++ layer-app/src/main/java/io/ctws/layer_app/services/EmployeeService.java
@@ -0,0 +1,24 @@
+package io.ctws.layer_app.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.ctws.layer_app.pojo.Employee;
+
+public class EmployeeService {
+
+	List<Employee> empList = new ArrayList<>();
+
+	public boolean addEmployee(Employee emp) {
+		return this.empList.add(emp);
+	}
+
+	public boolean addAllEmployee(List<Employee> empList) {
+		return this.empList.addAll(empList);
+	}
+
+	public Employee getEmplyeeById(long id) {
+		return (Employee) this.empList.stream().filter(emp -> emp.getId() == id);
+	}
+
+}
