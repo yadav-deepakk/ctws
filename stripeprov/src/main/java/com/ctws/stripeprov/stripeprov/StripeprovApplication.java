8bab18c5ed8a08aeb63ce4df9b420b6b50c0fe4f
yadav-deepakk <deepakyadav53882@gmail.com>
2026-01-01 16:58:43 +0530

    init stripe provider with docker compose again

---
+++ stripeprov/src/main/java/com/ctws/stripeprov/stripeprov/StripeprovApplication.java
@@ -0,0 +1,13 @@
+package com.ctws.stripeprov.stripeprov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeprovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripeprovApplication.class, args);
+	}
+
+}
---
+++ stripeprov/src/test/java/com/ctws/stripeprov/stripeprov/StripeprovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctws.stripeprov.stripeprov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeprovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
