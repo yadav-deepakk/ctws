21cb07508cf57bd1bc6eeb9928f8c9266302359e
yadav-deepakk <deepakyadav53882@gmail.com>
2026-01-18 23:56:08 +0530

    learn: TDD in-progress

---
+++ stripeprovider/src/main/java/com/ctws/payments/stripeprovider/controllers/Calculator.java
@@ -0,0 +1,11 @@
+package com.ctws.payments.stripeprovider.controllers;
+
+
+public class Calculator {
+
+  public long addTwoNumber(int a, int b) {
+    long sum = a + b ; 
+    return sum;
+  }
+  
+}
---
+++ stripeprovider/src/test/java/com/ctws/payments/stripeprovider/controllers/PaymentControllerTest.java
@@ -0,0 +1,22 @@
+package com.ctws.payments.stripeprovider.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ctws.payments.stripeprovider.services.interfaces.PaymentService;
+
+@WebMvcTest(PaymentController.class)
+public class PaymentControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc; 
+
+  @MockitoBean
+  private PaymentService paymentService; 
+
+  void shouldReturnPaymentResponse(){
+
+  }
+}
